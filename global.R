f356ea61afcfda8ba210cd8d90d1230a6445d63e
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2018-02-05 14:16:56 +0200

    Added plotly hovertext

--- global.R
+++ global.R
@@ -3,14 +3,16 @@ library(shinydashboard)
 library(ggvis)
 library(DT)
 library(tidyverse)
+library(plotly)
 
 data <- as.data.frame(readRDS("shiny_data.RDS"), stringsAsFactors = FALSE)
 
-metrics <- sort(c("times_cited", "recent_citations", "relative_citation_ratio", "field_citation_ratio", "oa", "fieldcount", "authors"))
+data <- data %>% 
+  mutate(text = paste0('<b>',parent,'</b>','<br>',name,'<br><b>',title,'</b><br>',journal_name,'<br>',publisher,'(',year,')','<br>Nr of fields: ',fieldcount,'<br>Nr of authors: ',authors,'<br>OA status in VIRTA: ',oa,'<br>OA full text?: ',is_oa))
+
+metrics <- sort(c("times_cited", "times_cited_wos", "recent_citations", "relative_citation_ratio", "field_citation_ratio", "oa", "fieldcount", "authors"))
 
 schools <- sort(unique(data$parent))
-
-scales <- c("linear", "log")
 
 
 
