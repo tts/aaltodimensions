5d8c98dabc03742bfdbc87b3861ed13c5f4590c0
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2018-02-05 14:18:54 +0200

    Changes to reflect ggvis->plotly and all schools selected by default

--- ui.R
+++ ui.R
@@ -7,26 +7,26 @@ sidebar <- dashboardSidebar(
       condition = "input.tabs == 'scatterplot'",
       selectInput(inputId = "school",
                   label = "school", 
-                  choices = schools,
+                  choices = c("All", schools),
                   multiple = FALSE,
-                  selected = "School of Science"),
+                  selected = "All"),
       selectInput(inputId = "dept",
                   label = "dept",
                   choices = NULL,
                   multiple = FALSE,
-                  selected = "All"),
+                  selected = NULL),
       selectInput("xc", "x axis", as.list(metrics), selected = "times_cited"),
-      selectInput("yc", "y axis", as.list(metrics), selected = "authors"),
-      selectInput("scale", "x axis scale", as.list(scales), selected = "linear"),
+      selectInput("yc", "y axis", as.list(metrics), selected = "times_cited_wos"),
       tags$div(class="form-group shiny-input-container",
-               HTML("<p>Citation metrics by <a target='blank' href='http://dimensions.ai'>Dimensions</a>, 
-                        Open Access full text evidence and URL by <a target='blank' href='http://unpaywall.org/api/v2'>Unpaywall</a>,
+               HTML("<p>Citation metrics by <a target='blank' href='http://dimensions.ai'>Dimensions</a> and Web of Science, 
+                        Open Access full text availabity info by <a target='blank' href='http://unpaywall.org/api/v2'>Unpaywall</a>,
                         article metadata by <a target='blank' href='https://confluence.csc.fi/display/VIR/REST-lukurajapinta'>VIRTA</a>,
                         Aalto University organisations by <a target='blank' href='http://research.aalto.fi'>local CRIS</a>.</p>
+                    <p><b>authors</b> = number of authors</p>
+                    <p><b>fieldcount</b> = number of different scientific fields</p>
                     <p>For citation metrics explanations, see <a href='https://figshare.com/articles/Dimensions_Metrics_API_Documentation/5783694'>Dimensions API Documentation</a></p>
-                    <p><b>VIRTA OA codes</b></br>0=Not OA</br>1=OA</br>2=Green OA</br>9=Not known</p>
                     <p>About this app, see <a href='https://blogs.aalto.fi/suoritin/'>this blog post (to come)</a></p>
-                    <p>Data as of 26 January 2018</p>")
+                    <p>Dimension data 26 Jan 2018, WoS data 4 Feb 2018</p>")
         )
       ),
     conditionalPanel(
@@ -35,8 +35,8 @@ sidebar <- dashboardSidebar(
                HTML("<p><b>authors</b> = number of authors</p>
                     <p><b>fieldcount</b> = number of different scientific fields</p>
                     <p>For citation metrics explanations, see <a href='https://figshare.com/articles/Dimensions_Metrics_API_Documentation/5783694'>Dimensions API Documentation</a></p>
-                    <p><b>VIRTA OA codes</b></br>0=Not OA</br>1=OA</br>2=Green OA</br>9=Not known</p>
-                    <p>About this app, see <a href='https://blogs.aalto.fi/suoritin/'>this blog post (to come)</a>")
+                    <p>About this app, see <a href='https://blogs.aalto.fi/suoritin/'>this blog post (to come)</a></p>
+                    <p>Dimension data 26 Jan 2018, WoS data 4 Feb 2018</p>")
       )),
     id = "tabs"
   )
@@ -50,14 +50,12 @@ body <- dashboardBody(
     tabItem("scatterplot",
             fluidRow(
               column(
-                width = 8,
-                box(title = "Scatterplot by unit",
-                    status = "success",
-                    solidHeader = TRUE,
-                    width = "100%",
-                    height = "700px",
-                    ggvisOutput("gv"))
-              )
+                width = 10,
+                heigth = "800px",
+                plotlyOutput("plotly",
+                             width = "100%",
+                             height = "800px")
+                )
               )
             ),
     
@@ -76,7 +74,7 @@ body <- dashboardBody(
 
 dashboardPage(
   dashboardHeader(title = "Citations and OA full text availability of Aalto University publications 2015-2017",
-                  titleWidth = "600"),
+                  titleWidth = "800"),
   sidebar,
   body,
   skin = "black"
