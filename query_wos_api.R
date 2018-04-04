f8b0929f79a7ce3f5e45060c385e4b137303c9df
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2018-04-04 11:02:12 +0300

    Initial commit

---
+++ query_wos_api.R
@@ -0,0 +1,10 @@
+library(wosr)
+library(tidyverse)
+
+Sys.setenv(WOS_USERNAME = "...", WOS_PASSWORD = "...")
+
+sid <- auth()
+q <- 'DO = 10.1002/jctb.4777'
+res <<- pull_wos(q, editions = c("SCI", "SSCI"))
+res$publication$tot_cites
+# 3
